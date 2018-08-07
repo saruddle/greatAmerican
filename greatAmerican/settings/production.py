bc954a10adb32d1aa5d5dac49c23afe74d6682f0
Scott Ruddle <saruddle@hotmail.com>
2018-08-07 13:47:52 -0400

    Set DIRS to 'template'

--- greatAmerican/settings/production.py
+++ greatAmerican/settings/production.py
@@ -63,7 +63,7 @@ ROOT_URLCONF = 'greatAmerican.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['app/templates/app'],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
